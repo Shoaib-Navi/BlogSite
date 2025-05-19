f2cdbe6a143ca9e995851e659305e7b297a8b665
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-05-19 23:40:58 +0530

    remove mongoose.connect from post model

--- model/post.js
+++ model/post.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://127.0.0.1:27017/BlogSite`)
-
 const postSchema = mongoose.schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
