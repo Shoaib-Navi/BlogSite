cdcccc83421865c1b9914a65cbe80b590072f353
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-05-19 13:54:23 +0530

    create user model- user.js

---
+++ model/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://127.0.0.1:27017/BlogSite`)
+
+const userSchema = mongoose.schema({
+    name:String,
+    username:String,
+    age:Number,
+    email:String,
+    password:String,
+    profilepic:{
+        type:String,
+        default:"default.png",
+    },
+    posts:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"post"
+    }]
+})
+
+module.exports = mongoose.model('user',userSchema);
